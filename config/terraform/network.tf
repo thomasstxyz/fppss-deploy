0ae11b9f13b1ed996f6094e3390f89fb457cc6fc
thomasstxyz <thomas@thomasst.xyz>
2022-10-07 16:14:54 +0200

    add terraform: EKS cluster

---
+++ config/terraform/network.tf
@@ -0,0 +1,28 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "example1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "example1"
+  }
+}
+
+resource "aws_subnet" "example2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "example2"
+  }
+}
